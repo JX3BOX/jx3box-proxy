416ef6dced54bc0362d5bc0ca45d230759f0587f
iRuxu <rx6@qq.com>
2020-03-06 22:25:19 +0800

    fix name

--- app/api/gsearch.js
+++ app/api/gsearch.js
@@ -1,7 +1,7 @@
 const Router = require("koa-router");
 const router = new Router();
 const gsearch_jsonapi = require('../model/gsearch_jsonapi');
-const gsearch_cse = require('../model/gsearch_cse');
+const gsearch_cseapi = require('../model/gsearch_cseapi');
 
 router.get("/gsearch/jsonapi", async (ctx, next) => {
     let data = "";
@@ -16,12 +16,12 @@ router.get("/gsearch/jsonapi", async (ctx, next) => {
     ctx.response.body = data;
 });
 
-router.get("/gsearch/cse", async (ctx, next) => {
+router.get("/gsearch/cseapi", async (ctx, next) => {
     let data = "";
     if (ctx.query.q) {
         let q = encodeURIComponent(ctx.query.q)
         console.log(`[gsearch/cseapi] parse query : ${q}`)
-        let res = await gsearch_cse(q)
+        let res = await gsearch_cseapi(q)
         // console.log(res.data)
         data = res ? res.data : ''
     }
